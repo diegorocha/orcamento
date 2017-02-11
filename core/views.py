eead0a3594b3ee281818985a6adcc0dfe573ed64
Diego Rocha <diego@diegorocha.com.br>
2017-02-11 21:27:13 -0200

    Reduzido para três itens no menu de orçamento e lista de compras

--- core/views.py
+++ core/views.py
@@ -7,14 +7,14 @@ from orcamento.models import Orcamento
 class UltimosOrcamentosMixin(object):
     def get_context_data(self, **kwargs):
         context = super(UltimosOrcamentosMixin, self).get_context_data(**kwargs)
-        context['ultimos_orcamentos'] = Orcamento.objects.all()[:6]
+        context['ultimos_orcamentos'] = Orcamento.objects.all()[:3]
         return context
 
 
 class UltimasListasMixin(object):
     def get_context_data(self, **kwargs):
         context = super(UltimasListasMixin, self).get_context_data(**kwargs)
-        context['ultimas_listas'] = ListaCompras.objects.all()[:6]
+        context['ultimas_listas'] = ListaCompras.objects.all()[:3]
         return context
 
 
