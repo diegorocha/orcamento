560a48c45b8e2d32bc03b5c9e43b9690f54dda72
Diego Rocha <diego@diegorocha.com.br>
2018-01-24 22:48:50 -0200

    Acertado PermissionRequired para direcionar para login quando não houver usuario logado

--- core/views.py
+++ core/views.py
@@ -14,6 +14,11 @@ class PermissionRequired(PermissionRequiredMixin):
             return ()  # Default é não precisar de nenhuma permissão
         return super(PermissionRequired, self).get_permission_required()
 
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            self.raise_exception = False
+        return super(PermissionRequired, self).handle_no_permission()
+
 
 class BaseViewMixin(LoginRequiredMixin, PermissionRequired):
     pass
