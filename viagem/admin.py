b8daea2043ee7ac08176fc0133e953eb3aa6f6c2
Diego Rocha <diego.rocha@m4u.com.br>
2020-02-23 00:51:35 -0300

    Criado cadastro para gastos de viagem

--- viagem/admin.py
+++ viagem/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from viagem import models
 
 
+class GastoViagemInline(admin.TabularInline):
+    extra = 0
+    model = models.GastoViagem
+
+
 @admin.register(models.Viagem)
 class OrcamentoAdmin(admin.ModelAdmin):
     list_filter = ['orcamento']
     list_display = ['__str__', 'inicio', 'fim']
+    inlines = [GastoViagemInline, ]
--- viagem/models.py
+++ viagem/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
-from orcamento.models import Orcamento
+from cartao.models import Fatura
+from orcamento.models import Orcamento, Categoria, Moeda
 
 
 @python_2_unicode_compatible
@@ -17,3 +18,30 @@ class Viagem(models.Model):
 
     def __str__(self):
         return self.descricao + self.inicio.strftime(' %Y/%m')
+
+
+@python_2_unicode_compatible
+class GastoViagem(models.Model):
+    class Meta:
+        verbose_name = 'Gasto Viagem'
+        verbose_name_plural = 'Gastos de Viagem'
+        ordering = ['dia', 'id']
+    viagem = models.ForeignKey(Viagem, on_delete=models.CASCADE, related_name='gastos')
+    dia = models.DateField('Dia', blank=False, null=False)
+    descricao = models.CharField('Descrição', max_length=50, blank=False, null=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='gastos_viagem')
+    valor_real = models.DecimalField('Valor em Reais', max_digits=8, decimal_places=2, blank=False, null=False)
+    valor_dolar = models.DecimalField('Valor em Dólares', max_digits=8, decimal_places=2, blank=True, null=True)
+    valor_moeda = models.DecimalField('Valor na moeda local', max_digits=8, decimal_places=2, blank=True, null=True)
+    moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE, related_name='gastos_viagem', blank=False)
+    taxa_conversao = models.DecimalField('Taxa de conversão', max_digits=8, decimal_places=3, blank=True, null=True)
+    fatura = models.ForeignKey(Fatura, on_delete=models.CASCADE, related_name='gastos_viagem', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.moeda:
+            # Set REAL as default
+            self.moeda = Moeda.objects.filter(sigla='BRL').first()
+        super(GastoViagem, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.descricao
