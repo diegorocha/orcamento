6e8bb38e3f87a455ae513e12c3a76ee90b432c8b
Diego Rocha <diego.rocha@m4u.com.br>
2021-05-04 00:25:59 -0300

    Update ecr lifecycle

--- terraform/main.tf
+++ terraform/main.tf
@@ -156,6 +156,29 @@ resource "aws_ecr_repository" "ecr_repository" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "ecr_repository_lifecycle" {
+  repository = aws_ecr_repository.ecr_repository.name
+
+  policy = jsonencode(
+    {
+      rules = [
+        {
+          action = {
+            type = "expire"
+          }
+          description  = "Keep last 3 images"
+          rulePriority = 1
+          selection = {
+            countNumber = 3
+            countType   = "imageCountMoreThan"
+            tagStatus   = "any"
+          }
+        },
+      ]
+    }
+  )
+}
+
 output "orcamento_bucket_name" {
   value = aws_s3_bucket.bucket_staticfiles.bucket
 }
