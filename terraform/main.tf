446e427eb91f108e41fc93254e6b8446a5675a39
Diego Rocha <diego.rocha@m4u.com.br>
2023-06-09 23:22:10 -0300

    Updating terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = "~>0.13"
+  required_version = "1.4.5"
+  required_providers { aws = { version = "5.1.0" } }
   backend "s3" {
     bucket               = "diegor-terraform"
     workspace_key_prefix = ""
